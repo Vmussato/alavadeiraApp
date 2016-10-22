d97f09d7bc579cb8796d2f85501dd67f1ad91ac7
Maicon Henrique <maiconhenriquekcond@gmail.com>
2016-10-22 09:35:29 -0200

    Projeto criado

---
+++ app/src/main/java/alavadeiraapp/com/example/maiconh/alavadeiraapp/MainActivity.java
@@ -0,0 +1,13 @@
+package alavadeiraapp.com.example.maiconh.alavadeiraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
